39b0dc9225c97b0a20cc087ab26fb197ec6916e3
MarcBeaudet <marc-antoine.beaudet@devinci.fr>
2018-03-26 21:37:12 +0100

    Added kill() function and people can vote only one time

--- voting/voting.sol
+++ voting/voting.sol
@@ -1,12 +1,37 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.4;
 
 contract Voting {
 
+  struct Voter
+    {
+      uint weight;
+      bool voted;  // if true, that person already voted
+      uint vote;   // index of the voted proposal
+    }
+
+  adress public chairperson;
+
   mapping (bytes32 => uint8) public votesReceived;
+  mapping(address => Voter) public voters;
   bytes32[] public candidateList;
 
+
   function Voting(bytes32[] candidateNames) public {
+    chairperson = msg.sender;
     candidateList = candidateNames;
+  }
+
+  // Give `voter` the right to vote on this ballot.
+  // May only be called by `chairperson`.
+  function giveRightToVote(address voter){
+      if (msg.sender != chairperson || voters[voter].voted)
+          // `throw` terminates and reverts all changes to
+          // the state and to Ether balances. It is often
+          // a good idea to use this if functions are
+          // called incorrectly. But watch out, this
+          // will also consume all provided gas.
+          throw;
+      voters[voter].weight = 1;
   }
 
   function totalVotesFor(bytes32 candidate) view public returns (uint8) {
@@ -16,6 +41,9 @@ contract Voting {
 
   function voteForCandidate(bytes32 candidate) public {
     require(validCandidate(candidate));
+    Voter sender = voters[msg.sender];
+    if (sender.voted) throw;
+    sender.voted = true;
     votesReceived[candidate]  += 1;
   }
 
@@ -26,5 +54,9 @@ contract Voting {
       }
     }
     return false;
+   }
+
+   function kill() public {
+      if(msg.sender == owner) selfdestruct(owner);
    }
 }
